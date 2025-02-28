3feee74f7169443b17153fa2be1ce95ab7d14725
borninthedark <princeton.strong@outlook.com>
2025-02-28 18:42:18 -0500

    update outputs

--- modules/application_gateway/outputs.tf
+++ modules/application_gateway/outputs.tf
@@ -2,3 +2,8 @@ output "app_gateway_id" {
   description = "The ID of the Application Gateway."
   value       = azurerm_application_gateway.appgw.id
 }
+
+output "app_gateway_public_ip" {
+  description = "The public IP of the Application Gateway."
+  value       = azurerm_application_gateway.appgw.frontend_ip_configuration[0].public_ip_address_id
+}
