f534ada3f655e4d7d4b0f71e42ace57d65cdf824
borninthedark <princeton.strong@outlook.com>
2025-02-28 22:17:13 -0500

    change provider host

--- provider.tf
+++ provider.tf
@@ -10,12 +10,11 @@ provider "kubernetes" {
 
 provider "helm" {
   kubernetes {
-    host                   = module.aks.kube_config_host
+    host                   = var.kube_config_host
     client_certificate     = base64decode(module.aks.kube_config_client_certificate)
     client_key             = base64decode(module.aks.kube_config_client_key)
     cluster_ca_certificate = base64decode(module.aks.kube_config_ca)
   }
 }
-
 
 
