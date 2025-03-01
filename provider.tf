f79151cf2c7ffc13e2fe03b622789f125311a3a4
borninthedark <princeton.strong@outlook.com>
2025-02-28 22:41:56 -0500

    zero no start

--- provider.tf
+++ provider.tf
@@ -8,13 +8,6 @@ provider "kubernetes" {
   token                  = module.aks.kube_config[0].token
 }
 
-provider "helm" {
-  kubernetes {
-    host                   = module.aks.kube_config[0].host
-    client_certificate     = base64decode(module.aks.kube_config[0].client_certificate)
-    client_key             = base64decode(module.aks.kube_config[0].client_key)
-    cluster_ca_certificate = base64decode(module.aks.kube_config_ca[0].cluster_ca_certificate)
-  }
-}
+provider "helm" {}
 
 
