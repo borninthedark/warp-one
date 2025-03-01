da217a3dcf3cec2e005183499001dc425fd7bfc4
borninthedark <princeton.strong@outlook.com>
2025-03-01 16:56:52 -0500

    kv ref fix

--- modules/keyvault/main.tf
+++ modules/keyvault/main.tf
@@ -19,8 +19,8 @@ resource "azurerm_key_vault" "keyvault" {
 resource "azurerm_key_vault_access_policy" "terraform_sp" {
   key_vault_id = azurerm_key_vault.keyvault.id
   tenant_id    = var.tenant_id
-  object_id    = var.sp_object_id 
-
+  object_id    = var.object_id  
+  
   key_permissions = [
     "Get", "List", "Update", "Create", "Import", "Delete", "Recover", "Backup", "Restore"
   ]
