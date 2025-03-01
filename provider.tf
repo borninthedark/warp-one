68c0435224b6dadfb9c99e4b59e5a4d6cf26d1a7
borninthedark <princeton.strong@outlook.com>
2025-02-28 22:36:36 -0500

    provider fixes

--- provider.tf
+++ provider.tf
@@ -3,17 +3,17 @@ provider "azurerm" {
 }
 
 provider "kubernetes" {
-  host                   = module.aks.kube_config_host[0].host
-  cluster_ca_certificate = base64decode(module.aks.kube_config_host[0].cluster_ca_certificate)
-  token                  = module.aks.kube_config_host[0].token
+  host                   = module.aks.kube_config[0].host
+  cluster_ca_certificate = base64decode(module.aks.kube_config[0].cluster_ca_certificate)
+  token                  = module.aks.kube_config[0].token
 }
 
 provider "helm" {
   kubernetes {
-    host                   = module.aks.kube_config_host[0].host
-    client_certificate     = base64decode(module.aks.kube_config_host_client_certificate)
-    client_key             = base64decode(module.aks.kube_config_host_client_key)
-    cluster_ca_certificate = base64decode(module.aks.kube_config_host_ca)
+    host                   = module.aks.kube_config[0].host
+    client_certificate     = base64decode(module.aks.kube_config[0].client_certificate)
+    client_key             = base64decode(module.aks.kube_config[0].client_key)
+    cluster_ca_certificate = base64decode(module.aks.kube_config_ca[0].cluster_ca_certificate)
   }
 }
 
