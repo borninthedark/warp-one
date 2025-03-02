977fe20e4ecfb3f25e54bd3de5968bab4d1d504b
borninthedark <princeton.strong@outlook.com>
2025-03-01 22:27:32 -0500

    provider auth config

--- provider.tf
+++ provider.tf
@@ -1,6 +1,13 @@
 provider "azurerm" {
   features {}
+
+  # Authentication via Service Principal 
+  client_id       = var.client_id
+  client_secret   = var.client_secret
+  tenant_id       = var.tenant_id
+  subscription_id = var.subscription_id
 }
+
 
 provider "kubernetes" {
   host                   = module.aks.kube_config_host
