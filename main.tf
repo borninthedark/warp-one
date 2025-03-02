73d8f45f73ec502e7b3eaa59e6d86c3100df7470
borninthedark <princeton.strong@outlook.com>
2025-03-01 23:32:44 -0500

    fmt fixes

--- main.tf
+++ main.tf
@@ -1,7 +1,7 @@
 # Resource Group
 module "resource_group" {
   source   = "./modules/resource_group"
-  name     = "rg-warp-one-${local.environment}"
+  name     = "rg-${local.environment}"
   location = local.location
 }
 
@@ -10,32 +10,32 @@ module "network" {
   source                        = "./modules/network"
   resource_group_name           = module.resource_group.resource_group_name
   location                      = module.resource_group.resource_group_location
-  vnet_name                     = "vnet-warp-one-${local.environment}"
+  vnet_name                     = "vnet-${local.environment}"
   vnet_address_space            = ["10.0.0.0/16"]
   aks_subnet_name               = "aks-subnet"
   aks_subnet_address_prefixes   = ["10.0.1.0/24"]
   appgw_subnet_name             = "appgw-subnet"
   appgw_subnet_address_prefixes = ["10.0.2.0/24"]
-  nsg_name                      = "nsg-warp-one-${local.environment}"
+  nsg_name                      = "nsg-${local.environment}"
   appgw_public_ip_name          = "appgw-public-ip-${local.environment}"
   tags = {
     environment = local.environment
-    project     = "warp-one"
+    project     = "phoenix"
   }
 }
 
 # Secrets & Key Vault
 module "secrets_management" {
   source              = "./modules/secrets_management"
-  name                = "kv-warp-one-${local.environment}"
+  name                = "kv-${local.environment}"
   resource_group_name = module.resource_group.resource_group_name
   location            = module.resource_group.resource_group_location
   object_id           = data.azurerm_client_config.current.object_id
   tenant_id           = data.azurerm_client_config.current.tenant_id
 
   ssl_certificate_name = "appgw-ssl-cert"
-  domain_name         = "princetonstrong.online"
-  validity_in_months  = 12
+  domain_name          = "princetonstrong.online"
+  validity_in_months   = 12
 
   secret_name  = var.secret_name
   secret_value = var.secret_value
@@ -51,7 +51,7 @@ module "dns" {
 # Log Analytics Workspace
 module "log_analytics" {
   source                = "./modules/log_analytics"
-  name                  = "log-warp-one-${local.environment}"
+  name                  = "log-${local.environment}"
   location              = module.resource_group.resource_group_location
   resource_group_name   = module.resource_group.resource_group_name
   sku                   = "PerGB2018"
@@ -59,7 +59,7 @@ module "log_analytics" {
   enable_aks_monitoring = true
   tags = {
     environment = local.environment
-    project     = "warp-one"
+    project     = "phoenix"
   }
 }
 
@@ -74,12 +74,12 @@ module "public_ip" {
 # Application Gateway 
 module "application_gateway" {
   source                    = "./modules/application_gateway"
-  name                      = "appgw-warp-one-${local.environment}"
+  name                      = "appgw-${local.environment}"
   location                  = module.resource_group.resource_group_location
   resource_group_name       = module.resource_group.resource_group_name
   public_ip_address_id      = module.public_ip.public_ip_id
   subnet_id                 = module.network.appgw_subnet_id
-  ssl_certificate_name      = module.secrets_management.ssl_certificate_name  
+  ssl_certificate_name      = module.secrets_management.ssl_certificate_name
   ssl_certificate_secret_id = module.secrets_management.certificate_secret_id
 }
 
@@ -94,7 +94,7 @@ module "acr" {
 # AKS Cluster 
 module "aks" {
   source                     = "./modules/aks"
-  name                       = "aks-warp-one-${local.environment}"
+  name                       = "aks-${local.environment}"
   location                   = module.resource_group.resource_group_location
   resource_group_name        = module.resource_group.resource_group_name
   dns_prefix                 = "akswarpone"
@@ -106,5 +106,5 @@ module "aks" {
   log_analytics_workspace_id = module.log_analytics.log_analytics_workspace_id
   acr_id                     = module.acr.acr_id
 
-  depends_on = [module.secrets_management]  
+  depends_on = [module.secrets_management]
 }
