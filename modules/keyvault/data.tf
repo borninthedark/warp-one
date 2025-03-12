f781c371c1873d39dfecced9ef20a622f53e5c1f
borninthedark <princeton.strong@outlook.com>
2025-03-12 15:31:17 -0400

    removed data resouce

--- modules/keyvault/data.tf
+++ modules/keyvault/data.tf
@@ -1,11 +1,6 @@
 # Get Current Azure Env Config 
 data "azurerm_client_config" "current" {}
 
-data "azurerm_key_vault_certificate" "nx" {
-  name         = "nx"
-  key_vault_id = azurerm_key_vault.keyvault.id
-}
-
 # Get the Azure AD Service Principal by its display name
 data "azuread_service_principal" "sp" {
   display_name = "tfc-service-principal"
