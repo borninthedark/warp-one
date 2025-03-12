48a8d2dc72e2d114d1af2e2ec0c12aa8552c2ff0
borninthedark <princeton.strong@outlook.com>
2025-03-12 14:55:26 -0400

    data fixes

--- data.tf
+++ data.tf
@@ -1,12 +1,3 @@
 # Get Current Azure Env Config 
 data "azurerm_client_config" "current" {}
 
-# Get the Azure AD Service Principal by its display name
-data "azuread_service_principal" "sp" {
-  display_name = "tfc-service-principal"  
-}
-
-# Output the Service Principal App ID
-output "service_principal_app_id" {
-  value = data.azuread_service_principal.sp.application_id
-}
--- modules/keyvault/data.tf
+++ modules/keyvault/data.tf
@@ -5,3 +5,9 @@ data "azurerm_key_vault_certificate" "nx" {
   name         = "nx"
   key_vault_id = azurerm_key_vault.keyvault.id
 }
+
+# Get the Azure AD Service Principal by its display name
+data "azuread_service_principal" "sp" {
+  display_name = "tfc-service-principal"  
+}
+
