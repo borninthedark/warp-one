b574fdb0da463c97e8f6b3c2ad90357c42fdbbd3
borninthedark <princeton.strong@outlook.com>
2025-03-12 14:42:59 -0400

    azuread?

--- modules/keyvault/main.tf
+++ modules/keyvault/main.tf
@@ -57,6 +57,57 @@ resource "azurerm_key_vault_access_policy" "module" {
   ]
 }
 
+resource "azurerm_key_vault_access_policy" "tfc" {
+  key_vault_id = azurerm_key_vault.keyvault.id
+  tenant_id    = var.tenant_id
+  object_id    = data.azuread_service_principal.sp.application_id
+
+  certificate_permissions = [
+    "Create",
+    "Delete",
+    "DeleteIssuers",
+    "Get",
+    "GetIssuers",
+    "Import",
+    "List",
+    "ListIssuers",
+    "ManageContacts",
+    "ManageIssuers",
+    "SetIssuers",
+    "Update",
+  ]
+
+  key_permissions = [
+    "Backup",
+    "Create",
+    "Decrypt",
+    "Delete",
+    "Encrypt",
+    "Get",
+    "Import",
+    "List",
+    "Purge",
+    "Recover",
+    "Restore",
+    "Sign",
+    "UnwrapKey",
+    "Update",
+    "Verify",
+    "WrapKey",
+  ]
+
+  secret_permissions = [
+    "Backup",
+    "Delete",
+    "Get",
+    "List",
+    "Purge",
+    "Recover",
+    "Restore",
+    "Set",
+  ]
+}
+
 resource "azurerm_key_vault_certificate" "nx" {
   name         = "appgw-nx-cert"
   key_vault_id = azurerm_key_vault.keyvault.id
